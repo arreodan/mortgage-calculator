dc37f80a3eae2e4531e4530e683c68cefedd516c
Daniel <arreoladani@yahoo.com>
2023-04-23 13:41:13 -0700

    displays 1 month of data

--- js/site.js
+++ js/site.js
@@ -2,8 +2,9 @@ function getInput(){
     let loanAmount = parseFloat(document.getElementById("loanAmount").value);
     let loanTerm = parseInt(document.getElementById("loanTerm").value);
     let interestRate = parseFloat(document.getElementById('interestRate').value);
+    let previousBalance = loanAmount;
 
-    calculateData(loanAmount, loanTerm, interestRate);
+    calculateData(loanAmount, loanTerm, interestRate, previousBalance);
 
     document.getElementById('monthlyPayment').innerText = ''
 
@@ -12,36 +13,33 @@ function getInput(){
     monthlyPayment = parseFloat(monthlyPayment).toFixed(2)
 
     document.getElementById('monthlyPayment').innerText = "$" + monthlyPayment;
-    document.getElementById("totalPrincipal").innerText = "$" + loanAmount;
+    document.getElementById("totalPrincipal").innerText = "$" + loanAmount; 
 
+    displayData(loanAmount, loanTerm, interestRate, previousBalance);
 }
 
-function calculateData(loanAmount, loanTerm, interestRate){
+function calculateData(loanAmount, loanTerm, interestRate, previousInterest, previousBalance){
 
     let monthlyPayment = loanAmount * (interestRate/1200) / (1-(1 + interestRate / 1200)**-loanTerm)
 
     
     let interestPayment = loanAmount * interestRate/1200;
-    // let totalInterest = interestPayment + previousInterest 
+    let totalInterest = interestPayment + previousInterest;
     let principalPayment = monthlyPayment - interestPayment;
-    let balance = loanAmount - principalPayment;
+    let balance = previousBalance - principalPayment;
 
     // create formulas to get total interest and previous balance
 
     let monthlyData = {
-        // month: month, 
-        payment: monthlyPayment,
-        principal: principalPayment,
-        interest: interestPayment,
-        // totalInterest: totalInterest,
-        balance: balance,
-    }
+      month: loanTerm,
+      payment: monthlyPayment.toFixed(2),
+      principal: principalPayment.toFixed(2),
+      interest: interestPayment.toFixed(2),
+      totalInterest: totalInterest.toFixed(2),
+      balance: balance.toFixed(2),
+    };
 
-    // create an array to store object 
-    let monthlyTermData = []
-
-    // store all objects 
-    monthlyTermData.push(monthlyData)
+    return monthlyData;
 }
 
 
@@ -49,26 +47,29 @@ function calculateData(loanAmount, loanTerm, interestRate){
 
 
 
-function displayData() {
+function displayData(loanAmount, loanTerm, interestRate, previousBalance){
     const dataTable = document.getElementById('monthlyData');
     const template = document.getElementById('tableRowTemplate');
+    let previousInterest = 0;
+    let remainingTerm = loanTerm;
 
     // use for loop to go through each object in the array from calculateData() 
     for(i = 0; i < loanTerm; i++){
-        let month = i
-
+        let monthlyData = calculateData(loanAmount, loanTerm, interestRate, previousInterest, previousBalance);
         let tableRow = document.importNode(template.content, true)
 
-        tableRow.querySelector('[data-id="month"]').textContent = month;
-        tableRow.querySelector('[data-id="payment"]').textContent = payment;
-        tableRow.querySelector('[data-id="principal"]').textContent = principal;
-        tableRow.querySelector('[data-id="interest"]').textContent = interest;
-        tableRow.querySelector('[data-id="totalInterest"]').textContent = totalInterest;
-        tableRow.querySelector('[data-id="balance"]').textContent = balance;
+        tableRow.querySelector('[data-id="month"]').textContent = i + 1;
+        tableRow.querySelector('[data-id="payment"]').textContent = monthlyData.payment;
+        tableRow.querySelector('[data-id="principal"]').textContent = monthlyData.principal;
+        tableRow.querySelector('[data-id="interest"]').textContent = monthlyData.interest;
+        tableRow.querySelector('[data-id="totalInterest"]').textContent = monthlyData.totalInterest;
+        tableRow.querySelector('[data-id="balance"]').textContent = monthlyData.balance;
 
 
         dataTable.appendChild(tableRow);
 
-        calculateData(month)
+        previousInterest = monthlyData.totalInterest;
+        previousBalance = monthlyData.balance;
+        remainingTerm = remainingTerm - 1;
     }
 }
